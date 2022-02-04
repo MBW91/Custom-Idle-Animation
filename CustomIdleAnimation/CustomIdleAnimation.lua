fc4f8af893c7f2f79c7bccd2f01aa266d54cef7b
Marvin Bernd Walters <mail@marvin-bernd-walters.de>
2022-02-04 12:45:05 +0900

    Updated to newest API version 101033

--- CustomIdleAnimation/CustomIdleAnimation.lua
+++ CustomIdleAnimation/CustomIdleAnimation.lua
@@ -2,7 +2,7 @@ CustomIdleAnimation = {
 	name = "CustomIdleAnimation",
 	title = "Custom Idle Animation",
 	author = "Xerrok",
-	version = "1.6.6",
+	version = "1.6.7",
 	savedVariablesVersion = 1.3
 }
 local CIA = CustomIdleAnimation
